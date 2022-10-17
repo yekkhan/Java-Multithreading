883039e52aa67b3ca897f9688e40aae12b228691
yekkhansiang <b160003@e.ntu.edu.sg>
2022-10-17 18:17:40 +0800

    init

---
+++ java-multithreading/src/test/java/com/example/javamultithreading/JavaMultithreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javamultithreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMultithreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
